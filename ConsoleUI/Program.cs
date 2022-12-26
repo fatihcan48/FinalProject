b59cf286a9e6848777c2f6338729ef737c523ab6
fatih <fatih@DESKTOP-F57A1SM>
2022-12-26 17:21:21 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,36 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using DataAccess.Concrete.InMemory;
+
+namespace ConsoleUI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CategoryTest();
+
+            Console.ReadKey();
+        }
+
+        private static void CategoryTest()
+        {
+            CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+
+            foreach (var item in categoryManager.GetAll())
+            {
+                Console.WriteLine(item.CategoryName);
+            }
+        }
+
+        private static void ProductTest()
+        {
+            ProductManager productManager = new ProductManager(new EfProductDal());
+
+            foreach (var item in productManager.GetAll())
+            {
+                Console.WriteLine(item.ProductName);
+            }
+        }
+    }
+}
---
+++ Core/DataAccess/IEntityRepository.cs
@@ -0,0 +1,27 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.DataAccess
+{
+    // Interface'imize her veri tipinin yazılmasını istemiyoruz. Sadece Entity nesneleri veilebilsin istiyoruz.
+    // Generic Constraint yaparak, filtreleme ekliyoruz. 
+    // new() eklememizin sebebi sadece new'lenebilen nesneleri yaz. Yani IEntity interface'ni yazama, implement eden classları yazabil...
+    public interface IEntityRepository<T> where T : class, IEntity, new()   //(Sadece IEntity class nesneleri seçilebilsin!!!)
+    {
+        // Delegate yapısı... (Expression)
+        List<T> GetAll(Expression<Func<T,bool>> filter =null);  // filter = null, filtre vermesemde çalışması için.
+        T Get(Expression<Func<T, bool>> filter);  
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+
+
+        // List<T> GetAllByCategory(int Id);
+        // Delegate yapısı ile buna gerek kalmadı. Çünkü istediğimiz filtreyi verebiliyoruz.
+    }
+}
